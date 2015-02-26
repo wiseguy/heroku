2b3a68bdb5d293719ddc87088d4ad362bd3899f4
Aamir Suleman <asuleman@blueraster.com>
2015-02-26 17:22:52 -0500

    static files support

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    res.sendfile('index.html')
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res) {
+    console.log('static file request : ' + req.params);
+    res.sendfile(__dirname + req.params[0]);
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
